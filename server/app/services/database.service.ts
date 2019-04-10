43597d19adfd1799944c896f9d11eb43950089c6
Nikolay <nikolay.radoev@polymtl.ca>
2019-04-10 19:15:24 -0400

    Removed repeated Pool connection

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,9 +1,9 @@
 import { injectable } from "inversify";
 import * as pg from "pg";
 import "reflect-metadata";
+import { Room } from "../../../common/tables/Room";
 import {schema} from "../createSchema";
 import {data} from "../populateDB";
-import { Room } from "../../../common/tables/Room";
 
 @injectable()
 export class DatabaseService {
@@ -20,43 +20,41 @@ export class DatabaseService {
 
     private pool: pg.Pool = new pg.Pool(this.connectionConfig);
 
+    public constructor() {
+        this.pool.connect();
+    }
     /*
 
         METHODES DE DEBUG
     */
     public createSchema(): Promise<pg.QueryResult> {
-        this.pool.connect();
-        
+
         return this.pool.query(schema);
     }
 
     public populateDb(): Promise<pg.QueryResult> {
-        this.pool.connect();
 
         return this.pool.query(data);
     }
 
     public getAllFromTable(tableName: string): Promise<pg.QueryResult> {
-        this.pool.connect();
 
         return this.pool.query(`SELECT * FROM HOTELDB.${tableName};`);
     }
 
     // HOTEL
     public getHotels(): Promise<pg.QueryResult> {
-        this.pool.connect();
 
         return this.pool.query('SELECT * FROM HOTELDB.Hotel;');
     }
 
     public getHotelNo(): Promise<pg.QueryResult> {
-        this.pool.connect();
 
         return this.pool.query('SELECT hotelNo FROM HOTELDB.Hotel;');
+
     }
 
     public createHotel(hotelNo: string, hotelName: string, city: string): Promise<pg.QueryResult> {
-        this.pool.connect();
         const values: string[] = [
             hotelNo,
             hotelName,
@@ -69,7 +67,6 @@ export class DatabaseService {
 
     // ROOM
     public getRoomFromHotel(hotelNo: string, roomType: string, price: number): Promise<pg.QueryResult> {
-        this.pool.connect();
 
         let query: string =
         `SELECT * FROM HOTELDB.room
@@ -88,7 +85,6 @@ export class DatabaseService {
     }
 
     public getRoomFromHotelParams(params: object): Promise<pg.QueryResult> {
-        this.pool.connect();
 
         let query: string = 'SELECT * FROM HOTELDB.room \n';
         const keys: string[] = Object.keys(params);
@@ -115,7 +111,6 @@ export class DatabaseService {
     }
 
     public createRoom(room: Room): Promise<pg.QueryResult> {
-        this.pool.connect();
         const values: string[] = [
             room.roomno,
             room.hotelno,
@@ -133,7 +128,7 @@ export class DatabaseService {
                        guestName: string,
                        gender: string,
                        guestCity: string): Promise<pg.QueryResult> {
-        this.pool.connect();
+        // this.pool.connect();
         const values: string[] = [
             guestNo,
             nas,
@@ -152,7 +147,6 @@ export class DatabaseService {
                          dateFrom: Date,
                          dateTo: Date,
                          roomNo: string): Promise<pg.QueryResult> {
-        this.pool.connect();
         const values: string[] = [
             hotelNo,
             guestNo,
